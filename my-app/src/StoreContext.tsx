c22b67bae6241005d72ccccddbdc271db9b0778b
Sergey-Kupchik <kupchikrabota@gmail.com>
2020-08-17 13:54:52 -0400

    fixed PostsStatePropsType and containers comp

--- my-app/src/StoreContext.tsx
+++ my-app/src/StoreContext.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import {store} from './redux/reduxStore';
 import { StoreType } from "./redux/state";
+import App from './App';
+import {BrowserRouter} from 'react-router-dom';
 
 
 
-export const StoreContext = React.createContext<StoreType>(store)
+export const StoreContext = React.createContext<StoreType>(store);
