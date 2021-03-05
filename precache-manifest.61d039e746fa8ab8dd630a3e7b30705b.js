a4ca0ca2f855fe4243b875fbe82faaea0b8410e0
Sergey-Kupchik <kupchikrabota@gmail.com>
2021-03-05 12:32:40 -0500

    Updates

---
+++ precache-manifest.61d039e746fa8ab8dd630a3e7b30705b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9f856b7778bf69ad4bee214ac57f5f2",
+    "url": "/Social_Media/index.html"
+  },
+  {
+    "revision": "dfd3040a140791f7f495",
+    "url": "/Social_Media/static/css/main.89c4b1f9.chunk.css"
+  },
+  {
+    "revision": "6005f6d9bac7c8c14b41",
+    "url": "/Social_Media/static/js/2.9d487788.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Social_Media/static/js/2.9d487788.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfd3040a140791f7f495",
+    "url": "/Social_Media/static/js/main.358c668e.chunk.js"
+  },
+  {
+    "revision": "14cf354a816e51f785f8",
+    "url": "/Social_Media/static/js/runtime-main.adb8e20d.js"
+  },
+  {
+    "revision": "fc12e14344e65d9b2b7ccf1f88514778",
+    "url": "/Social_Media/static/media/logoRF.fc12e143.png"
+  },
+  {
+    "revision": "66bab914396c23b083ec74bad45d7c8d",
+    "url": "/Social_Media/static/media/logoRF_mobile.66bab914.png"
+  },
+  {
+    "revision": "c5af975309b09ffaf06c8ac255de76ec",
+    "url": "/Social_Media/static/media/preloader.c5af9753.svg"
+  },
+  {
+    "revision": "908d8322ef3042702bf61843abfd3c8e",
+    "url": "/Social_Media/static/media/user.908d8322.png"
+  }
+]);
